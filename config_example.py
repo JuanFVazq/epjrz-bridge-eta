7e2150d8820b77e6ab147f30f547c7228c092b64
JuanFVazq <juanfvazquezolivas@gmail.com>
2025-10-11 14:16:56 -0600

    Config File Example

---
+++ config_example.py
@@ -0,0 +1,6 @@
+CBP_URL = "https://bwt.cbp.gov/xml/bwt.xml"
+GOOGLE_SERVER_KEY = "YOUR API KEY HERE"
+GOOGLE_BROWSER_KEY = "YOUR API KEY HERE"
+
+#APIs for Server Key: Geocoding API, Distance Matrix API
+#APIs for Browser Key: Maps JavaScript API, Place API, Places API (New)
